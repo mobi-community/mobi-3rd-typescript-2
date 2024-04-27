ae82f558c03cbd52624a9a73cb663c5f062227c8
hayoung <wendy4332@naver.com>
2024-04-28 03:33:36 +0900

    feat: 1.ts 수정

--- task3/src/question/components/one-todo.tsx
+++ task3/src/question/components/one-todo.tsx
@@ -1,22 +1,38 @@
-import { TodoDataBase, TodoEnum } from "@type/todo";
+import { TodoDataBase } from "@type/todo";
 
 interface Props {
     data: TodoDataBase;
 }
 
+// const OneTodo: React.FC<Props> = ({ data }) => {
+//     switch (data.type) {
+//         case TodoEnum.DAILY:
+//             return (
+//                 <>
+//                     <div>{data.title}</div>
+//                     <div>{data.content}</div>
+//                 </>
+//             );
+//         case TodoEnum.WEEKLY:
+//             return <div>{data.total.toDateString()}</div>;
+//         case TodoEnum.MONTHLY:
+//             return <div>{data.goal}</div>;
+//     }
+// };
+// export default OneTodo;
+
 const OneTodo: React.FC<Props> = ({ data }) => {
-    switch (data.type) {
-        case TodoEnum.DAILY:
-            return (
-                <>
-                    <div>{data.title}</div>
-                    <div>{data.content}</div>
-                </>
-            );
-        case TodoEnum.WEEKLY:
-            return <div>{data.total.toDateString()}</div>;
-        case TodoEnum.MONTHLY:
-            return <div>{data.goal}</div>;
+    if ("title" in data && "content" in data) {
+        return (
+            <>
+                <div>{data.title}</div>
+                <div>{data.content}</div>
+            </>
+        );
+    } else if ("total" in data) {
+        return <div>{data.total.toDateString()}</div>;
+    } else if ("goal" in data) {
+        return <div>{data.goal}</div>;
     }
 };
 export default OneTodo;
